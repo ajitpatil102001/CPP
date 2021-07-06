cac00f47e7ac70319fbe2af23a4dcd76e8736712
Ajit Patil <54498483+ajitpatil102001@users.noreply.github.com>
2021-07-06 09:51:40 +0530

    Add files via upload

---
+++ area_of_rectangle.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	float b,h,area_of_tringle;
+	cout<<"Enter a height=";
+	cin>>h;
+	cout<<"Enter a breadth=";
+	cin>>b;
+	area_of_tringle=0.5*(b*h);
+	cout<<"Area of tringle="<<area_of_tringle;
+	return 0;
+}
